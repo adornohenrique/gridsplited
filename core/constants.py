1a9e234806b6ea70d831debb2a868f00bd26f853
adornohenrique <henriquenobreadorno@hotmail.com>
2025-09-25 10:11:31 +0200

    Create constants.py

---
+++ core/constants.py
@@ -0,0 +1,20 @@
+# core/constants.py
+DEFAULTS = {
+    "PLANT_CAP_MW": 20.0,
+    "MIN_LOAD_PCT": 10.0,
+    "MAX_LOAD_PCT": 100.0,
+    "BREAK_EVEN_EUR_MWH": 50.0,
+    "RAMP_LIMIT_MW": 2.0,
+    "ALWAYS_ON": True,
+    "MWH_PER_TON": 11.0,
+    "MEOH_PRICE": 1000.0,
+    "CO2_PRICE": 40.0,
+    "CO2_INTENSITY": 1.375,
+    "MAINT_PCT": 3.0,
+    "SGA_PCT": 2.0,
+    "INS_PCT": 1.0,
+    "WATER_COST_T": 7.3,
+    "TRADER_MARGIN_PCT_UI": 10.0,
+    "OTHER_OPEX_T": 0.0,
+    "TARGET_MARGIN_PCT": 30.0,
+}
